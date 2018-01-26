d9986a454b11cb5b488fac6b243919ba9f8f4c12
c1505 <coreym247@gmail.com>
2018-01-26 02:32:50 +0000

    time_clock has a stop time

--- babysitter.rb
+++ babysitter.rb
@@ -1,6 +1,6 @@
 class TimeClock
   attr_reader :start_time
-  attr_writer :stop_time
+  attr_accessor :stop_time
   
   def initialize(start_time)
     @start_time = start_time
@@ -11,6 +11,7 @@ class BabysitterCLI
   def self.run
     print "Please enter your start time (ex. 5:00PM)"
     time_clock = TimeClock.new(gets.chomp)
+    print "Please enter your stop time"
+    time_clock.stop_time = gets.chomp
   end
-
 end
--- babysitter_test.rb
+++ babysitter_test.rb
@@ -9,5 +9,11 @@ class BabysitterTest < Minitest::Test
     time_clock = TimeClock.new("5:00PM")
     assert_equal "5:00PM", time_clock.start_time
   end
+  
+  def test_has_a_stop_time
+    time_clock = TimeClock.new("5:00PM")
+    time_clock.stop_time = "8:00PM"
+    assert_equal "8:00PM", time_clock.stop_time
+  end
 
 end
